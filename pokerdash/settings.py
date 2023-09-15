a82ae937fbf2082a799af0083124055d03e4858d
nbain00 <zink47@gmail.com>
2023-09-14 19:43:34 -0600

    database change

--- pokerdash/settings.py
+++ pokerdash/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'pokerdash.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'database/db.sqlite3',
     }
 }
 
